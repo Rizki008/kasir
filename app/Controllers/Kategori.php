123aa8bf7a010510f8c28478e09a61c9a5b749ce
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-01-19 11:43:37 +0700

    membuat crud kategori

--- app/Controllers/Kategori.php
+++ app/Controllers/Kategori.php
@@ -3,9 +3,14 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ModelKategori;
 
 class Kategori extends BaseController
 {
+    public function __construct()
+    {
+        $this->ModelKategori = new ModelKategori();
+    }
     public function index()
     {
         $data = [
@@ -13,8 +18,42 @@ class Kategori extends BaseController
             'subjudul' => 'Kategori',
             'menu' => 'masterdata',
             'submenu' => 'kategori',
-            'page' => 'v_kategori'
+            'page' => 'v_kategori',
+            'kategori' => $this->ModelKategori->alldata(),
         ];
         return view('v_template', $data);
+    }
+
+    public function insertdata()
+    {
+        $data = ['nama_kategori' => $this->request->getPost('nama_kategori')];
+
+        $this->ModelKategori->insertdata($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Ditambahkan');
+
+        return redirect()->to('Kategori');
+    }
+    public function updatedata($id_kategori)
+    {
+        $data = [
+            'id_kategori' => $id_kategori,
+            'nama_kategori' => $this->request->getPost('nama_kategori'),
+        ];
+
+        $this->ModelKategori->updatedata($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Diedit');
+
+        return redirect()->to('Kategori');
+    }
+    public function deletedata($id_kategori)
+    {
+        $data = [
+            'id_kategori' => $id_kategori,
+        ];
+
+        $this->ModelKategori->deletedata($data);
+        session()->setFlashdata('pesan', 'Data Berhasil DIhapus');
+
+        return redirect()->to('Kategori');
     }
 }
