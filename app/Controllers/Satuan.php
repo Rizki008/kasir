6989dfebd68ccc423fd5e1cff27aa64c3efcf777
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-01-19 11:31:34 +0700

    membuat crud satuan

--- app/Controllers/Satuan.php
+++ app/Controllers/Satuan.php
@@ -3,9 +3,14 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Modelsatuan;
 
 class Satuan extends BaseController
 {
+    public function __construct()
+    {
+        $this->Modelsatuan = new Modelsatuan();
+    }
     public function index()
     {
         $data = [
@@ -13,8 +18,42 @@ class Satuan extends BaseController
             'subjudul' => 'Satuan',
             'menu' => 'masterdata',
             'submenu' => 'satuan',
-            'page' => 'v_satuan'
+            'page' => 'v_satuan',
+            'satuan' => $this->Modelsatuan->alldata(),
         ];
         return view('v_template', $data);
+    }
+
+    public function insertdata()
+    {
+        $data = ['nama_satuan' => $this->request->getPost('nama_satuan')];
+
+        $this->Modelsatuan->insertdata($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Ditambahkan');
+
+        return redirect()->to('Satuan');
+    }
+    public function updatedata($id_satuan)
+    {
+        $data = [
+            'id_satuan' => $id_satuan,
+            'nama_satuan' => $this->request->getPost('nama_satuan'),
+        ];
+
+        $this->Modelsatuan->updatedata($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Diedit');
+
+        return redirect()->to('Satuan');
+    }
+    public function deletedata($id_satuan)
+    {
+        $data = [
+            'id_satuan' => $id_satuan,
+        ];
+
+        $this->Modelsatuan->deletedata($data);
+        session()->setFlashdata('pesan', 'Data Berhasil DIhapus');
+
+        return redirect()->to('Satuan');
     }
 }
--- app/Views/v_satuan.php
+++ app/Views/v_satuan.php
@@ -1,1 +1,124 @@
-<h1>Satuan</h1>
+<div class="col-md-12">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title"><?= $subjudul ?></h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-toggle="modal" data-target="#add-data"><i class="fas fa-plus"></i>Tambah Data
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <?php
+            if (session()->getFlashdata('pesan')) {
+                echo '<div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <h5><i class="icon fas fa-check"></i>';
+                echo session()->getFlashdata('pesan');
+                echo '</h5></div>';
+            }
+
+            ?>
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th width="50px">No</th>
+                        <th>Satuan</th>
+                        <th width="150px">Aksi</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($satuan as $key => $value) { ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $value["nama_satuan"] ?></td>
+                            <td>
+                                <button type="button" class="btn btn-warning btn-sm btn-flat" data-toggle="modal" data-target="#edit-data<?= $value['id_satuan'] ?>"><i class="fa fa-pencil-alt"></i></button>
+                                <button type="button" class="btn btn-danger btn-sm btn-flat" data-toggle="modal" data-target="#delete-data<?= $value['id_satuan'] ?>"><i class="fa fa-trash-alt"></i></button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!-- add data -->
+<div class="modal fade" id="add-data">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah <?= $subjudul ?></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php echo form_open('satuan/insertdata') ?>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="">Nama Satuan</label>
+                    <input name="nama_satuan" class="form-control" placeholder="Satuan" required>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary btn-flat">Simpan</button>
+            </div>
+            <?php echo form_close() ?>
+        </div>
+    </div>
+</div>
+
+<!-- edit data -->
+<?php foreach ($satuan as $key => $value) { ?>
+    <div class="modal fade" id="edit-data<?= $value['id_satuan'] ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Edit <?= $subjudul ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php echo form_open('satuan/updatedata/' . $value['id_satuan']) ?>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nama Satuan</label>
+                        <input name="nama_satuan" value="<?= $value['nama_satuan'] ?>" class="form-control" placeholder="Satuan" required>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-warning btn-flat">Simpan</button>
+                </div>
+                <?php echo form_close() ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<!-- hapus data -->
+<?php foreach ($satuan as $key => $value) { ?>
+    <div class="modal fade" id="delete-data<?= $value['id_satuan'] ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Delete <?= $subjudul ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah Anda Yakin Akan Hapus Satuan <?= $value['nama_satuan'] ?>???</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                    <a href="<?= base_url('Satuan/deletedata/' . $value['id_satuan']) ?>" class="btn btn-danger btn-flat">Hapus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
