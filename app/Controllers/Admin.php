97f0c784881bf2fe8fcf4092db579a6deb448058
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-01-19 09:31:52 +0700

    membuat menu aktif

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -9,6 +9,10 @@ class Admin extends BaseController
     public function index()
     {
         $data = [
+            'judul' => 'Dashboard',
+            'subjudul' => '',
+            'menu' => 'dashboard',
+            'submenu' => '',
             'page' => 'v_admin'
         ];
         return view('v_template', $data);
