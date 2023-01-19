4c337d02d972062e4a6627c6ac6804068c236261
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-01-19 11:44:08 +0700

    membuat view crud kategori

--- app/Views/v_kategori.php
+++ app/Views/v_kategori.php
@@ -1,1 +1,124 @@
-<h1>Produk</h1>
+<div class="col-md-12">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title"><?= $subjudul ?></h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-toggle="modal" data-target="#add-data"><i class="fas fa-plus"></i>Tambah Data
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <?php
+            if (session()->getFlashdata('pesan')) {
+                echo '<div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <h5><i class="icon fas fa-check"></i>';
+                echo session()->getFlashdata('pesan');
+                echo '</h5></div>';
+            }
+
+            ?>
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th width="50px">No</th>
+                        <th>Nama Kategori</th>
+                        <th width="150px">Aksi</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($kategori as $key => $value) { ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $value["nama_kategori"] ?></td>
+                            <td>
+                                <button type="button" class="btn btn-warning btn-sm btn-flat" data-toggle="modal" data-target="#edit-data<?= $value['id_kategori'] ?>"><i class="fa fa-pencil-alt"></i></button>
+                                <button type="button" class="btn btn-danger btn-sm btn-flat" data-toggle="modal" data-target="#delete-data<?= $value['id_kategori'] ?>"><i class="fa fa-trash-alt"></i></button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!-- add data -->
+<div class="modal fade" id="add-data">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah <?= $subjudul ?></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php echo form_open('kategori/insertdata') ?>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="">Nama Satuan</label>
+                    <input name="nama_kategori" class="form-control" placeholder="Kategori" required>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary btn-flat">Simpan</button>
+            </div>
+            <?php echo form_close() ?>
+        </div>
+    </div>
+</div>
+
+<!-- edit data -->
+<?php foreach ($kategori as $key => $value) { ?>
+    <div class="modal fade" id="edit-data<?= $value['id_kategori'] ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Edit <?= $subjudul ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php echo form_open('kategori/updatedata/' . $value['id_kategori']) ?>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nama Kategori</label>
+                        <input name="nama_kategori" value="<?= $value['nama_kategori'] ?>" class="form-control" placeholder="Kategori" required>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-warning btn-flat">Simpan</button>
+                </div>
+                <?php echo form_close() ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<!-- hapus data -->
+<?php foreach ($kategori as $key => $value) { ?>
+    <div class="modal fade" id="delete-data<?= $value['id_kategori'] ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Delete <?= $subjudul ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah Anda Yakin Akan Hapus Kategori <?= $value['nama_kategori'] ?>???</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                    <a href="<?= base_url('Kategori/deletedata/' . $value['id_kategori']) ?>" class="btn btn-danger btn-flat">Hapus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
