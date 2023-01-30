30c11e02f2efd8bb3234b8de5bab1ed99b1883cd
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-01-30 15:36:20 +0700

    mengubah view produk

--- app/Views/v_produk.php
+++ app/Views/v_produk.php
@@ -1,1 +1,317 @@
-<h1>Produk</h1>
+<div class="col-md-12">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title"><?= $subjudul ?></h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-toggle="modal" data-target="#add-data"><i class="fas fa-plus"></i>Tambah Data
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <?php
+
+            use Kint\Zval\Value;
+
+            if (session()->getFlashdata('pesan')) {
+                echo '<div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <h5><i class="icon fas fa-check"></i>';
+                echo session()->getFlashdata('pesan');
+                echo '</h5></div>';
+            }
+
+            ?>
+
+            <?php
+            $errors = session()->getFlashdata('errors');
+            if (!empty($errors)) { ?>
+                echo '<div class="alert alert-danger alert-dismissible">
+                    <h4>Periksa Kembali Form Inputan !!!</h4>
+                    <ul>
+                        <?php foreach ($errors as $key => $error) { ?>
+                            <li><?= esc($error) ?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            <?php } ?>
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr class="text-center">
+                        <th width="50px">No</th>
+                        <th>Kode Produk</th>
+                        <th>Nama Produk</th>
+                        <th>Kategori Produk</th>
+                        <th>Satuan</th>
+                        <th>Harga Beli</th>
+                        <th>Harga Jual</th>
+                        <th>Stock</th>
+                        <th width="150px">Aksi</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($produk as $key => $value) { ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $value["kode_produk"] ?></td>
+                            <td><?= $value["nama_produk"] ?></td>
+                            <td><?= $value["nama_kategori"] ?></td>
+                            <td><?= $value["nama_satuan"] ?></td>
+                            <td class="text-center">
+                                Rp. <?= number_format($value["harga_beli"], 0)  ?>
+                            </td>
+                            <td class="text-center">
+                                Rp. <?= number_format($value["harga_jual"], 0) ?>
+                            </td>
+                            <td class="text-center">
+                                <?php if ($value['stok'] < 20) { ?>
+                                    <span class="bagde badge-danger">
+                                        <?= $value["stok"] ?>
+                                    </span>
+                                <?php } elseif ($value['stok'] > 21) { ?>
+                                    <span class="bagde badge-success">
+                                        <?= $value["stok"] ?>
+                                    </span>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-warning btn-sm btn-flat" data-toggle="modal" data-target="#edit-data<?= $value['id_produk'] ?>"><i class="fa fa-pencil-alt"></i></button>
+                                <button type="button" class="btn btn-danger btn-sm btn-flat" data-toggle="modal" data-target="#delete-data<?= $value['id_produk'] ?>"><i class="fa fa-trash-alt"></i></button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+<!-- add data -->
+<div class="modal fade" id="add-data">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah <?= $subjudul ?></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php echo form_open('produk/insertdata') ?>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="">Kode Produk</label>
+                    <input name="kode_produk" class="form-control" value="<?= old('kode_produk') ?>" placeholder="Kode Produk" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Nama Produk</label>
+                    <input name="nama_produk" class="form-control" value="<?= old('nama_produk') ?>" placeholder="Nama Produk" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Kategori</label>
+                    <select name="id_kategori" id="id_kategori" class="form-control">
+                        <option value="">---Pilih Kategori---</option>
+                        <?php foreach ($kategori as $key => $value) { ?>
+                            <option value="<?= $value['id_kategori'] ?>"><?= $value['nama_kategori'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="">Satuan</label>
+                    <select name="id_satuan" id="id_satuan" class="form-control">
+                        <option value="">---Pilih Satuan---</option>
+                        <?php foreach ($satuan as $key => $value) { ?>
+                            <option value="<?= $value['id_satuan'] ?>"><?= $value['nama_satuan'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="">Harga Beli</label>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Rp.</span>
+                        </div>
+                        <input name="harga_beli" id="harga_beli" class="form-control" value="<?= old('harga_beli') ?>" placeholder="Harga Beli" required>
+                        <div class="input-group-append">
+                            <span class="input-group-text">.00</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="">Harga Jual</label>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Rp.</span>
+                        </div>
+                        <input name="harga_jual" id="harga_jual" class="form-control" value="<?= old('harga_jual') ?>" placeholder="Harga Jual" required>
+                        <div class="input-group-append">
+                            <span class="input-group-text">.00</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="">Stock</label>
+                    <input name="stok" type="number" class="form-control" value="<?= old('stok') ?>" placeholder="Stock" required>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary btn-flat">Simpan</button>
+            </div>
+            <?php echo form_close() ?>
+        </div>
+    </div>
+</div>
+
+<?php
+foreach ($produk as $key => $value) { ?>
+    <div class="modal fade" id="edit-data<?= $value['id_produk'] ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Edit <?= $subjudul ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php echo form_open('produk/updatedata/' . $value['id_produk']) ?>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Kode Produk</label>
+                        <input name="kode_produk" class="form-control" value="<?= $value['kode_produk'] ?>" placeholder="Kode Produk" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Nama Produk</label>
+                        <input name="nama_produk" class="form-control" value="<?= $value['nama_produk'] ?>" placeholder="Nama Produk" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Kategori</label>
+                        <select name="id_kategori" id="id_kategori" class="form-control">
+                            <option value="">---Pilih Kategori---</option>
+                            <?php foreach ($kategori as $key => $k) { ?>
+                                <option value="<?= $k['id_kategori'] ?>" <?= $value['id_kategori'] == $k['id_kategori'] ? 'Selected' : '' ?>><?= $k['nama_kategori'] ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Satuan</label>
+                        <select name="id_satuan" id="id_satuan" class="form-control">
+                            <option value="">---Pilih Satuan---</option>
+                            <?php foreach ($satuan as $key => $s) { ?>
+                                <option value="<?= $s['id_satuan'] ?>" <?= $value['id_satuan'] == $s['id_satuan'] ? 'Selected' : '' ?>><?= $s['nama_satuan'] ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Harga Beli</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Rp.</span>
+                            </div>
+                            <input name="harga_beli" id="harga_beli<?= $value['id_produk'] ?>" class="form-control" value="<?= $value['harga_beli'] ?>" placeholder="Harga Beli" required>
+                            <div class="input-group-append">
+                                <span class="input-group-text">.00</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Harga Jual</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Rp.</span>
+                            </div>
+                            <input name="harga_jual" id="harga_jual<?= $value['id_produk'] ?>" class="form-control" value="<?= $value['harga_jual'] ?>" placeholder="Harga Jual" required>
+                            <div class="input-group-append">
+                                <span class="input-group-text">.00</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Stock</label>
+                        <input name="stok" type="number" class="form-control" value="<?= $value['stok'] ?>" placeholder="Stock" required>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-primary btn-flat">Simpan</button>
+                </div>
+                <?php echo form_close() ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<!-- hapus data -->
+<?php foreach ($produk as $key => $value) { ?>
+    <div class="modal fade" id="delete-data<?= $value['id_produk'] ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Delete <?= $subjudul ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah Anda Yakin Akan Hapus Produk <?= $value['nama_produk'] ?>???</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Tutup</button>
+                    <a href="<?= base_url('produk/deletedata/' . $value['id_produk']) ?>" class="btn btn-danger btn-flat">Hapus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<!-- Page specific script -->
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
+            "paging": true,
+            "ordering": false,
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        // $('#example2').DataTable({
+        //     "paging": true,
+        //     "lengthChange": false,
+        //     "searching": false,
+        //     "ordering": true,
+        //     "info": true,
+        //     "autoWidth": false,
+        //     "responsive": true,
+        // });
+    });
+
+    new AutoNumeric('#harga_beli', {
+        // currencySymbol: 'Rp.'
+        digitGroupSeparator: ',',
+        // decimalCharacter: '.',
+        // decimalCharacterAlternative: '.',
+        decimalPlaces: 0,
+    });
+    new AutoNumeric('#harga_jual', {
+        // currencySymbol: 'Rp.'
+        digitGroupSeparator: ',',
+        // decimalCharacter: '.',
+        // decimalCharacterAlternative: '.',
+        decimalPlaces: 0,
+    });
+
+    <?php
+    foreach ($produk as $key => $value) { ?>
+        new AutoNumeric('#harga_beli<?= $value['id_produk'] ?>', {
+            digitGroupSeparator: ',',
+            decimalPlaces: 0,
+        });
+        new AutoNumeric('#harga_jual<?= $value['id_produk'] ?>', {
+            digitGroupSeparator: ',',
+            decimalPlaces: 0,
+        });
+    <?php } ?>
+</script>
